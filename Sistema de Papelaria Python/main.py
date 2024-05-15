a77c4315d6209e0be6e8d827913800695aafc8ca
Emanuel Victor Brebal <manu.brebal@gmail.com>
2024-05-15 13:37:28 -0300

    menu

--- Sistema de Papelaria Python/main.py
+++ Sistema de Papelaria Python/main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from usuarios.users_functions import insert_user, login
+from usuarios.users_functions import insert_user, login, list_user, list_user_ByID
 
 #estabelece a conexão com o banco de dados
 conexao = sqlite3.connect('papelaria_db.sqlite')
@@ -11,19 +11,30 @@ cursor = conexao.cursor()
 while(True):
     print("""
         1- Cadastrar Usuario
-        2- Entrar no Sistema
+        2- Realizar o login 
+        3- Mostrar todos os Usuários
+        4- Mostrar Usuário por ID 
     """)
     opc = int(input("Digite a oção: "))
-    if opc == 1:
-        username = str(input("Digite o email: "))
+    if opc == 1: #cadastro de usuarios
+        username = str(input("Digite o username: "))
         password = str(input("Digite a senha: "))
         insert_user(username, password)
     
-    elif opc == 2:
-        username = str(input("Digite o email: "))
+    elif opc == 2: # entrar no sistema
+        username = str(input("Digite o username: "))
         password = str(input("Digite a senha: "))
         autenticado = login(username, password)
         if autenticado:
-            print(autenticado)
+            print("Usuário autenticado")
             break
+        else:
+            print("Usuário ou senha incorreto(s), por favor tente novamente.")
+
+    elif opc == 3: #mostrar todos os usuários
+        list_user()
+
+    elif opc == 4: #mostrar usuário por ID
+        new_ID = int(input("Digite o ID do usuário: "))
+        list_user_ByID(new_ID)
         
--- Sistema de Papelaria Python/usuarios/users_functions.py
+++ Sistema de Papelaria Python/usuarios/users_functions.py
@@ -6,39 +6,41 @@ conexao = sqlite3.connect('papelaria_db.sqlite')
 cursor = conexao.cursor()
 
 #CREATE USER
-def insert_user(email, password):
+def insert_user(username, password):
     cursor.execute("""
-        INSERT INTO users(email, password) VALUES (?, ?)
-    """, [email, password])
+        INSERT INTO users(username, password) VALUES (?, ?)
+    """, [username, password])
     conexao.commit()
 
 #UTILIZAR PARA ALIMENTAR A FUNÇÃO E CHAMAR NO ARQUIVO MAIN
-new_email = str(input("Digite seu email: "))
-new_password = str(input("Digite sua senha: "))
-insert_user(new_email, new_password)
+
+#new_email = str(input("Digite seu email: "))
+#new_password = str(input("Digite sua senha: "))
+#insert_user(new_email, new_password)
 
 #READ USER
 def list_user():
     all_users = cursor.execute("""
-        SELECT email FROM users
+        SELECT user_ID, username FROM users
     """).fetchall()
     print(all_users)
 
 #UTILIZAR QUANDO FOR CHAMAR A FUNÇÃO DE LISTAR USUÁRIOS
-list_user()
+#list_user()
 
 #READ USER BY ID
 def list_user_ByID(user_ID):
     users_ByID= cursor.execute("""
-        SELECT email FROM users WHERE user_ID = ?
+        SELECT user_ID, username FROM users WHERE user_ID = ?
     """, [user_ID]).fetchone()
     print(users_ByID)
 
 #UTILIZAR QUANDO FOR CHAMAR A FUNÇÃO DE LISTAR USUÁRIOS
-new_ID = int(input("Digite o ID do usuário: "))
-list_user_ByID(new_ID)
+
+#new_ID = int(input("Digite o ID do usuário: "))
+#list_user_ByID(new_ID)
 
 #FUNÇÃO RESPONSÁVEL PELO LOGIN
-def login(email, password):
-    sql = "SELECT * FROM users WHERE password = ? AND password = ?"
-    return cursor.execute(sql, [email, password]).fetchone()
+def login(username, password):
+    sql = "SELECT * FROM users WHERE username = ? AND password = ?"
+    return cursor.execute(sql, [username, password]).fetchone()
